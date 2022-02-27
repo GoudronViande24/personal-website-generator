23a95c7c9c08e54b04de682190c66eb364a82a2b
Artivain <thomas@artivain.tk>
2022-02-26 20:57:18 -0500

    Begin the sidebar customization

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+import axios from "https://cdn.skypack.dev/axios";
+import showdown from "https://cdn.skypack.dev/showdown";
 const octokit = new Octokit();
+const converter = new showdown.Converter();
 
-// Input section
+// Import section
 const githubButton = document.getElementById("githubButton");
 
 // Form section
+const form = {};
+[
+	"name",
+	"nickname",
+	"avatar",
+	"facebook",
+	"instagram",
+	"youtube",
+	"twitch",
+	"discord",
+	"linkedin",
+	"paypal",
+	"pinterest",
+	"reddit",
+	"snapchat",
+	"telegram",
+	"twitter",
+	"github",
+	"stack-overflow",
+	"vimeo"
+].forEach(i => form[i] = document.getElementById(i));
 
-
-// Handle the imports
+// Import from GitHub
 githubButton.addEventListener("click", async () => {
 	const input = document.getElementById("githubInput").value;
 	if (!githubInput.value) {
@@ -17,14 +40,24 @@ githubButton.addEventListener("click", async () => {
 	try {
 
 		const githubUser = await octokit.request(`GET /users/${input}`);
+
+		form.name.value = githubUser.data.name;
+		form.nickname.value = githubUser.data.login;
+		form.avatar.value = githubUser.data.avatar_url;
+		form.github.value = githubUser.data.html_url;
+
 		try {
 			var githubUserRepo = await octokit.request(`GET /repos/${input}/${input}`);
 		} catch (e) {
 			// Rethrow non-404 error code
 			if (e != "HttpError: Not Found") throw e;
 		};
-		
-		console.log("ok");
+
+		if (githubUserRepo) {
+			var readme = await axios.get(`https://raw.githubusercontent.com/${githubUserRepo.data.full_name}/${githubUserRepo.data.default_branch}/README.md`);
+		};
+
+
 
 	} catch (e) {
 		if (e == "HttpError: Not Found") return alert("User not found.");
