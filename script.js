c52919c916df478c6e2a7c90eb7097fbc9bfed62
Artivain <thomas@artivain.tk>
2022-02-26 23:33:09 -0500

    About me section

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 import axios from "https://cdn.skypack.dev/axios";
 import showdown from "https://cdn.skypack.dev/showdown";
 const octokit = new Octokit();
-const converter = new showdown.Converter();
+const converter = new showdown.Converter({
+	simplifiedAutoLink: true,
+	underline: true,
+	openLinksInNewWindow: true,
+	backslashEscapesHTMLTages: true,
+	emoji: true,
+	ghMentions: true,
+	headerLevelStart: 3,
+	noHeaderId: true
+});
+converter.setFlavor("github");
 
 // Import section
 const githubButton = document.getElementById("githubButton");
@@ -27,7 +37,12 @@ const form = {};
 	"twitter",
 	"github",
 	"stack-overflow",
-	"vimeo"
+	"vimeo",
+	"about-me",
+	"about-me-mode",
+	"about-me-mode-text",
+	"about-me-mode-md",
+	"about-me-mode-html"
 ].forEach(i => form[i] = document.getElementById(i));
 
 // Import from GitHub
@@ -48,16 +63,27 @@ githubButton.addEventListener("click", async () => {
 
 		try {
 			var githubUserRepo = await octokit.request(`GET /repos/${input}/${input}`);
+			if (githubUserRepo) {
+				var readme = await axios.get(`https://raw.githubusercontent.com/${githubUserRepo.data.full_name}/${githubUserRepo.data.default_branch}/README.md`);
+			};
 		} catch (e) {
 			// Rethrow non-404 error code
 			if (e != "HttpError: Not Found") throw e;
 		};
 
-		if (githubUserRepo) {
-			var readme = await axios.get(`https://raw.githubusercontent.com/${githubUserRepo.data.full_name}/${githubUserRepo.data.default_branch}/README.md`);
+		if (readme) {
+			form["about-me"].innerText = readme.data;
+			form["about-me-mode-md"].checked = true;
+			updateAboutMeMode();
+		} else if (githubUser.data.bio) {
+			form["about-me"].innerText = githubUser.data.bio;
+			form["about-me-mode-text"].checked = true;
+			updateAboutMeMode();
 		};
 
+		const githubPinned = await axios.get(`https://github-pinned-repo-api.herokuapp.com/?username=${githubUser.data.login}`);
 
+		
 
 	} catch (e) {
 		if (e == "HttpError: Not Found") return alert("User not found.");
@@ -65,3 +91,37 @@ githubButton.addEventListener("click", async () => {
 		console.error(e);
 	};
 });
+
+// About me
+form["about-me"].addEventListener("keyup", updateAboutMePreview);
+form["about-me-mode"].addEventListener("click", updateAboutMeMode);
+
+/**
+ * Update the "About me" preview
+ */
+async function updateAboutMePreview() {
+	const preview = document.getElementById("about-me-preview");
+	const mode = document.querySelector("input[name='about-me-mode']:checked").value;
+
+	if (mode == "text") {
+		preview.innerText = form["about-me"].innerText;
+	} else if (mode == "md") {
+		preview.innerHTML = converter.makeHtml(form["about-me"].innerText);
+	} else {
+		preview.innerHTML = form["about-me"].innerText;
+	};
+};
+
+/**
+ * Update the "About me" mode
+ */
+async function updateAboutMeMode() {
+	const mode = document.querySelector("input[name='about-me-mode']:checked").value;
+	if (mode == "text") {
+		form["about-me"].classList.remove("font-monospace");
+	} else {
+		form["about-me"].classList.add("font-monospace");
+	};
+
+	updateAboutMePreview();
+};
