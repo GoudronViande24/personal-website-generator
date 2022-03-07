e62f7dfc5d1e97cd9dbc8d1a4077a65f5fdf3052
Artivain <thomas@artivain.tk>
2022-03-06 22:04:06 -0500

    Projects section

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ converter.setFlavor("github");
 // Import section
 const githubButton = document.getElementById("githubButton");
 
-// Form section
+/**
+ * Object containing all the parts of the form
+ * @type {Object.<string, Element>}
+ * 
+ */
 const form = {};
 [
 	"name",
@@ -42,7 +46,11 @@ const form = {};
 	"about-me-mode",
 	"about-me-mode-text",
 	"about-me-mode-md",
-	"about-me-mode-html"
+	"about-me-mode-html",
+	"projects-add",
+	"project-template",
+	"projects",
+	"projects-preview"
 ].forEach(i => form[i] = document.getElementById(i));
 
 // Import from GitHub
@@ -81,9 +89,26 @@ githubButton.addEventListener("click", async () => {
 			updateAboutMeMode();
 		};
 
+		// Import projects
 		const githubPinned = await axios.get(`https://github-pinned-repo-api.herokuapp.com/?username=${githubUser.data.login}`);
+		if (githubPinned.data) {
+			await removeAllProjects();
+			githubPinned.data.forEach(async (pin) => {
+				const languagesRequest = await octokit.request(`GET /repos/${pin.owner}/${pin.repo}/languages`);
+				const languages = [];
+				for (const language in languagesRequest.data) languages.push(language);
+				const project = addProject();
 
-		
+				project.querySelector("input.project-name").value = pin.repo;
+				project.querySelector("input.project-icon").value = "github";
+				project.querySelector("input.project-description").value = (pin.description ? pin.description : "");
+				project.querySelector("input.project-skills").value = (languages ? languages.join(", ") : "");
+				project.querySelector("input.project-url").value = pin.link;
+				project.querySelector("input.project-thumbnail").value = pin.image;
+
+				updateProjectsPreview();
+			});
+		};
 
 	} catch (e) {
 		if (e == "HttpError: Not Found") return alert("User not found.");
@@ -125,3 +150,89 @@ async function updateAboutMeMode() {
 
 	updateAboutMePreview();
 };
+
+/**
+ * List of all projects
+ * @type {Element[]}
+ */
+const projects = [];
+
+// Add a project button listener
+form["projects-add"].addEventListener("click", addProject);
+
+/**
+ * Add a project to the list
+ * @param {Boolean} updatePreview - Update or not the preview
+ * @returns {Element} - The new project in the form
+ */
+function addProject(updatePreview) {
+	const clone = form["project-template"].cloneNode(true);
+	clone.id = "";
+	const newProject = form["projects"].appendChild(clone);
+	projects.push(newProject);
+	newProject.querySelector("button.project-remove").addEventListener("click", removeProject, { once: true });
+	// Set event listeners for the inputs to refresh the preview
+	[
+		"project-name",
+		"project-icon",
+		"project-description",
+		"project-skills",
+		"project-url",
+		"project-thumbnail"
+	].forEach(item => newProject.querySelector(`input.${item}`).addEventListener("keyup", updateProjectsPreview));
+	if (updatePreview) updateProjectsPreview();
+	return newProject
+};
+
+/**
+ * Remove a project from the list
+ * @param {Event} event
+ */
+async function removeProject(event) {
+	projects.forEach((project, i) => {
+		if (project == event.target.parentNode) projects.splice(i, 1);
+	});
+	event.target.parentNode.remove();
+	updateProjectsPreview();
+};
+
+/**
+ * Remove all projects in the form
+ */
+async function removeAllProjects() {
+	projects.forEach(project => project.remove());
+	projects.length = 0;
+	updateProjectsPreview();
+};
+
+/**
+ * Update the preview of the projects section
+ */
+function updateProjectsPreview() {
+	const previews = [];
+
+	projects.forEach(project => {
+		previews.push(`
+		<a class="text-reset text-decoration-none"
+			href="${project.querySelector("input.project-url").value}" target="_blank">
+			<div class="card mb-3">
+				<div class="card-img-top"
+					style="background-image: url('${project.querySelector("input.project-thumbnail").value}');"></div>
+				<div class="card-body">
+					<h5 class="card-title">
+						<i class="bi bi-${project.querySelector("input.project-icon").value.toLowerCase()} me-3"></i>${project.querySelector("input.project-name").value}
+					</h5>
+					<p class="card-text">
+						${project.querySelector("input.project-description").value}
+					</p>
+					<p class="card-text">
+						<small class="text-muted">${project.querySelector("input.project-skills").value}</small>
+					</p>
+				</div>
+			</div>
+		</a>
+		`);
+	});
+
+	form["projects-preview"].innerHTML = previews.join("\n");
+}; 
